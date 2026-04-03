b2447395ef1033d138f0b61983a651c479956812
MurthiNext <MurthiNext@qq.com>
2026-04-03 20:56:19 +0800

    添加写入到文件

--- img2text.py
+++ img2text.py
@@ -3,14 +3,10 @@ from braille import image_to_braille
 from asciichr import image_to_asciichr
 
 if __name__ == "__main__":
-    img_path = '1x.png'
+    img_path = '1.png'
     width = 50
 
     ascii_result = image_to_asciichr(
-        image_path=img_path,
-        output_width=width
-    )
-    color_blocks_result = image_to_color_blocks(
         image_path=img_path,
         output_width=width
     )
@@ -18,5 +14,15 @@ if __name__ == "__main__":
         image_path=img_path,
         output_width=width
     )
-    for a, b, c in zip(ascii_result, color_blocks_result, braille_result):
+    color_blocks_result = image_to_color_blocks(
+        image_path=img_path,
+        output_width=width
+    )
+    for a, b, c in zip(ascii_result, braille_result, color_blocks_result):
         print(a + "   " + b + "   " + c)
+    with open('ascii_result.txt', 'w') as a:
+        a.write('\n'.join(ascii_result))
+    with open('braille_result.txt', 'w') as b:
+        b.write('\n'.join(braille_result))
+    with open('color_blocks_result.txt', 'w') as c:
+        c.write('\n'.join(color_blocks_result))
